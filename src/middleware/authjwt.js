5bf1bf0b6a387bf9793b5f0cbf780c6bdf5e64ae
Jeancorne <ctgi0101@gmail.com>
2021-01-16 13:07:41 -0500

    Subiendo

---
+++ src/middleware/authjwt.js
@@ -0,0 +1,28 @@
+const auth = {}
+const jwt = require('jsonwebtoken');
+const modelUsuario = require('../Models/usuariomodel.js');
+auth.tokenverificar = async (req, res, next) => {
+    try {
+        const token = req.headers["x-access-token"];
+        if (!token) {
+            return res.status(403).json({ message: "Debe enviar el token." });
+        }
+        const newjwt = jwt.verify(token, "namasecrett");        
+        console.log(newjwt);
+        const user = await modelUsuario.findById(newjwt.id);
+        console.log(user);
+        if (!user) {     
+            return res.status(404).json({message:"Usuario no encontrado."});
+        }
+        next();
+    } catch (error) {
+        return res.status(404).json({message:"Token no es valido."});
+    }
+
+}
+
+
+
+module.exports = auth;
+
+
